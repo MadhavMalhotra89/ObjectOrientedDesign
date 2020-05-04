756ea0567f664ced49ef1ed23be9064aed682e5c
MadhavMalhotra89 <55325147+MadhavMalhotra89@users.noreply.github.com>
2020-05-04 18:50:09 +0530

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -13,3 +13,37 @@ Class LRU:
     self.tail = LinkedList(0, 0)
     self.head.next = self.tail
     self.tail.prev = self.head
+    
+  def get(self, key):
+    if key in self.lru
+      
+    else:
+      return -1
+  def put(self, key, value):
+    
+    
+  def add(self, key, value):
+    if key in self.lru and self.lru[key] == value:
+      return self.lru[key]
+    if key in self.lru and self.lru[key] != value:
+      remove(key)
+      add(key, value)
+      self.lru[key] = value
+    elif len(self.lru) == self.capacity:
+      node = self.head.next
+      self._remove(node.key)
+      self._add(key,value)
+    else:
+      self._add(key,value)
+  def remove(self, key):
+    temp = self.head
+    while True:
+      if temp.key == key:
+        temp1 = temp.next
+        temp2 = temp.prev
+        temp.prev.next  = temp1
+        temp.next.prev = temp2
+        del self.lru[key]
+        break
+      else:
+        temp = temp.next
